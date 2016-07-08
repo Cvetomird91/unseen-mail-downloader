523389930b246538a0590777bd1fc9dd5e1094ff
Cvetomird91 <cvetomirdenchev@gmail.com>
2016-07-08 10:10:13 +0300

    added main function and abstracted functionality into separate functions

--- unseen.js
+++ unseen.js
@@ -10,24 +10,39 @@ $('.Conv2MsgHeader').each(function(key, value) {
 http://stackoverflow.com/questions/7717851/save-file-javascript-with-file-name
 */
 
-url = 'https://webmail.unseen.is/h/printmessage?id=281&tz=Europe/Athens'
-xhr = new XMLHttpRequest();
-xhr.open('GET', url, false);
-xhr.send();
-doc = xhr.responseText;
-parser = new DOMParser();
-mailPage = parser.parseFromString(doc, 'text/html');
+var url = 'https://webmail.unseen.is/h/printmessage?id=281&tz=Europe/Athens'
 
-//remove JavaScript that starts print dialog
-printDialog = mailPage.getElementsByTagName('script')[4];
-printDialog.parentNode.removeChild(printDialog);
+function fetchMessage(messageUrl) {
+	
+	xhr = new XMLHttpRequest();
+	xhr.open('GET', url, false);
+	xhr.send();
+	doc = xhr.responseText;
+	parser = new DOMParser();
+	mailPage = parser.parseFromString(doc, 'text/html');
 
-//download file
-uriContent = "data:application/octet-stream;filename=filename.html," + encodeURIComponent(mailPage.head.innerHTML + mailPage.body.innerHTML);
-a = document.createElement('a');
-a.download = 'filename.html';
-a.href = uriContent;
-a.style.display = 'hidden';
-document.body.appendChild(a);
-a.click();
-a.parentNode.removeChild(a);
+	//remove JavaScript that starts print dialog
+	printDialog = mailPage.getElementsByTagName('script')[4];
+	printDialog.parentNode.removeChild(printDialog);
+
+	return mailPage;
+
+}
+
+function downloadMessage(messageHTML, filename) {
+	uriContent = "data:application/octet-stream;filename=filename.html," + encodeURIComponent(messageHTML.head + messageHTML.body);
+	a = document.createElement('a');
+	a.download = 'filename.html';
+	a.href = uriContent;
+	a.style.display = 'hidden';
+	document.body.appendChild(a);
+	a.click();
+	a.parentNode.removeChild(a);
+}
+
+function main() {
+	messageHTML = fetchMessage(url);
+	downloadMessage(messageHTML);
+}
+
+main();
