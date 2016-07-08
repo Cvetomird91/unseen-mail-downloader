cd58fd1061c8f70d63412218d4f9298a6a059b53
Cvetomird91 <cvetomirdenchev@gmail.com>
2016-07-08 09:54:49 +0300

    added file download with custom filename

--- unseen.js
+++ unseen.js
@@ -1,13 +1,12 @@
-
 /*
 $('.Conv2MsgHeader').each(function(key, value) {
     element = $('.Conv2MsgHeader')[key];
     $(element).click();
-    setTimeout(function(){}, 10000);
+    setTimeout(function(){console.log('ok')}, 10000);
 });
 */
 
-/* Save file in JS 
+/* Save file in JS
 http://stackoverflow.com/questions/7717851/save-file-javascript-with-file-name
 */
 
@@ -15,7 +14,6 @@ url = 'https://webmail.unseen.is/h/printmessage?id=281&tz=Europe/Athens'
 xhr = new XMLHttpRequest();
 xhr.open('GET', url, false);
 xhr.send();
-xhr.responseText;
 doc = xhr.responseText;
 parser = new DOMParser();
 mailPage = parser.parseFromString(doc, 'text/html');
@@ -25,18 +23,11 @@ printDialog = mailPage.getElementsByTagName('script')[4];
 printDialog.parentNode.removeChild(printDialog);
 
 //download file
-//uriContent = "data:application/octet-stream;filename=filename.html," + encodeURIComponent(mailPage.head.innerHTML + mailPage.body.innerHTML);
-//newWindow = window.open(uriContent, 'filename.html');
-
-//blob
-var blob = new Blob([mailPage.head.innerHTML + mailPage.body.innerHTML], {type: 'octet/stream'});
-
-/*
-var anchor = document.createElement('a');
-document.body.appendChild(anchor);
-anchor.style = 'display:none';
-url = window.URL.createObjectUrl(blob);
-anchor.download = 'mail-message.html';
-anchor.click();
-window.URL.revokeObjectURL(url);
-*/
+uriContent = "data:application/octet-stream;filename=filename.html," + encodeURIComponent(mailPage.head.innerHTML + mailPage.body.innerHTML);
+a = document.createElement('a');
+a.download = 'filename.html';
+a.href = uriContent;
+a.style.display = 'hidden';
+document.body.appendChild(a);
+a.click();
+a.parentNode.removeChild(a);
